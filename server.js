aae0ff86779aafda7fed8166cdc7ecdffe6fbadf
Katie Bug <katierosealford@gmail.com>
2024-04-18 21:45:52 -0400

    made initial files and getting set up with installs

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mysql = require('mysql2');
+const prompt = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'Employee Manager'
+  },
+  console.log(`Connected to the Employee Manager database.`)
+);
