905427b3668226977a888a404f3d5d0b4d011f5e
Katie Bug <katierosealford@gmail.com>
2024-04-19 19:04:50 -0400

    removing express as it is not needed

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
-const express = require('express');
 const mysql = require('mysql2');
 const prompt = require('inquirer');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const newConnection = mysql.createConnection(
@@ -74,7 +67,4 @@ const newConnection = mysql.createConnection(
   }
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
   
