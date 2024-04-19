9fd76d3d2bf9a6bd4aafdde179f6a5e59e4fbd8c
Katie Bug <katierosealford@gmail.com>
2024-04-19 19:33:50 -0400

    created schema not sure if it is right but a work in progress!

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employeeManager_db;
+CREATE DATABASE employeeManager_db;
+USE employeeManager_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100),
+    salary DECIMAL(8,2),
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    role_id INT,
+    manager_id INT NOT NULL
+);
