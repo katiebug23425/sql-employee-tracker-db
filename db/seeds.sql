cc0d78ebf237aa55f1289a8db51a4124299a82bb
Katie Bug <katierosealford@gmail.com>
2024-04-19 19:49:04 -0400

    started the seeds file for employee names

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO departments (department_name)
+VALUES
+()
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+()
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+("Adrianna", "Poleni", ),
+("Lilly", "Moore", ),
+("Julia", "Magnan", ),
+("Diane", "Skoczylas", ),
+("Joseph", "Keller", ),
+("Patrick", "Schultz", ),
+("Amanda", "Stewart", ),
+("Marcella", "Roberts", ),
+("Gordon", "Smith", ),
+("Michael", "Williams", );
