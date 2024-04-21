1818794b4322bc51697effb5450590bbbbf26197
Katie Bug <katierosealford@gmail.com>
2024-04-21 07:35:48 -0400

    created addRole function. I LOVE our Virtual Asst, it is a lifesaver!

--- server.js
+++ server.js
@@ -135,6 +135,48 @@ async function allRoles() {
 }
 
 
+// function to add a role
+async function addRole() {
+    try {
+        const query = "SELECT * FROM departments";
+        const res = await newConnection.query(query);
+
+        const response = await prompt([
+            {
+                type: "input",
+                name: "title",
+                message: "Please enter the title of the role you would like to create:",
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "Please enter the salary of the role you would like to create:",
+            },
+            {
+                type: "list",
+                name: "department",
+                message: "Please select the department for the role you are creating:",
+                choices: res.map((department) => department.department_name),
+            },
+        ]);
+
+        const department = res.find((department) => department.name === response.department);
+        
+        const insertQuery = "INSERT INTO roles SET ?";
+        await newConnection.query(insertQuery, {
+            title: response.title,
+            salary: response.salary,
+            department_id: department.id,
+        });
+
+        console.log(`You have successfully added role ${response.title} to the ${response.department} department with salary ${response.salary} to the database!`);
+        startApp();
+    } catch (err) {
+        console.error("Error adding role:", err);
+        startApp();
+    }
+}
+
 
 
 // close the connection when the application exits
