abb3d0ae0955ac730fb16bb1bc3a2c10f1febe08
Katie Bug <katierosealford@gmail.com>
2024-04-25 11:32:48 -0400

    added cfonts to make the console look good

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2/promise');
 const util = require('util');
 const { prompt } = require('inquirer');
 const chalk = require('chalk'); 
+const cfonts = require('cfonts');
 
 let db;
 (async () => {
@@ -19,6 +20,21 @@ let db;
       console.error(chalk.red('Error connecting to the database:', err));
     }
  })();
+
+ cfonts.say("Katie Rose's \n Employee Tracker", {
+	font: 'block',              // define the font face
+	align: 'center',              // define text alignment
+	colors: ['candy'],         // define all colors
+	background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+	letterSpacing: 1,           // define letter spacing
+	lineHeight: 1,              // define the line height
+	space: true,                // define if the output text should have empty lines on top and on the bottom
+	maxLength: '0',             // define how many character can be on one line
+	gradient: false,            // define your two gradient colors
+	independentGradient: false, // define if you want to recalculate the gradient for each new line
+	transitionGradient: false,  // define if this is a transition between colors directly
+	env: 'node'                 // define the environment cfonts is being executed in
+});
 
   // Function to Start Employee Tracker Application
   async function startApp() {
